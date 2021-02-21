e3cb7857863eb5447e701cd84a65d083111fa299
Jeremy Guo <70536890+jeremyg39@users.noreply.github.com>
2021-02-21 14:55:27 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+min = 1
+max = 6
+print("Rolling the dice")
+print(random.randint(1, 6))
+roll_again = input("Roll again?")
+
+while roll_again == "yes":
+    print("Rolling again...")
+    print (random.randint(1, 6))
+    break
+
+
