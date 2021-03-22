86cd983258a3c055bc805709681935c070f6b7e6
Jeremy Guo <70536890+jeremyg39@users.noreply.github.com>
2021-03-21 20:34:37 -0700

    removed that random break

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ min = 1
 max = 6
 print("Rolling the dice")
 print(random.randint(1, 6))
-roll_again = input("Roll again?")
+roll_again = input("Roll again? ")
 
 if roll_again == "yes":
     print("Rolling again...")
     print (random.randint(1, 6))
-    break
-
-
+else:
+    print("done")
